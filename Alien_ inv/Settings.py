2f0c466ca5ef1a4f2fb7682f5a74123e5f180529
andrewsokolovski <72194996+andrewsokolovski@users.noreply.github.com>
2021-08-02 19:26:38 +0300

    added color of background

---
+++ Alien_ inv/Settings.py
@@ -0,0 +1,12 @@
+class Settings():
+    """ Class  for kepping all settings of Allien invasion"""
+
+
+
+    def __init__(self):
+        """ there is initialazing settings of game"""
+        #Screen parameters
+        self.screen_widgth = 1200
+        self.screen_heights = 800
+        #Color of becgraund
+        self.bg_color = (230, 230, 230)
--- Alien_ inv/alien_invasion.py
+++ Alien_ inv/alien_invasion.py
@@ -2,37 +2,45 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
-    """  Class for management of game resoerses """
+    """  Class for management of game resourses """
     def __init__(self):
         """ That is initialaizing game and create resourses of game"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
-        #Color of becgraund
-        self.bg_color = (230, 230, 230)
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_widgth, self.settings.screen_heights))
+
+        #self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion my first program")
+
+        self.ship = Ship(self.screen)
+
+        
 
     def run_game(self):
         """ Launch the mane game"""
         while True:
             # Follow events of keyboard and mouth
             for event in pygame.event.get():
-                if event.type == pygame.QUITE:
+                if event.type == pygame.QUIT:
                     sys.exit()
 
                 # Each cicle run the scree redraw
-                self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
 
             #Reflects the last drawing screen.
             pygame.display.flip()
 
 
-if __name__== '__main__':
+if __name__ == '__main__':
     # instance create and launch the game
     ai = AlienInvasion()
     ai.run_game()
-    
-    
