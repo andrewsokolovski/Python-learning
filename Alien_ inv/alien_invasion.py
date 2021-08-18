9eefbbcd39670cce67dd4aae21c0a04fdf0dbf33
andrewsokolovski <72194996+andrewsokolovski@users.noreply.github.com>
2021-08-18 17:55:14 +0300

    Merge remote-tracking branch 'origin/master'
    added particle at the center
    made refactoring
    added ship moving to the right/left

--- Alien_ inv/alien_invasion.py
+++ Alien_ inv/alien_invasion.py
@@ -31,6 +31,7 @@ class AlienInvasion:
         """ Launch the mane game"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -41,7 +42,15 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     # move the sheep to the right.
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
 
 
     def _update_screen(self):
--- Alien_ inv/ship.py
+++ Alien_ inv/ship.py
@@ -11,9 +11,20 @@ class Ship():
 		self.image = pygame.image.load('images/ship1.jpg')
 		self.image = pygame.transform.scale(self.image, (80, 70))
 		self.rect = self.image.get_rect()
-		#Every ship shows up at the bottom of the edge 
+
+		#Every ship shows up at the bottom of the edge
 		self.rect.midbottom = self.screen_rect.midbottom
 
+		#Flag of moving
+		self.moving_right = False
+		self.moving_left = False
+
+	def update(self):
+		""" Update ship position in according to the flag"""
+		if self.moving_right:
+			self.rect.x += 1
+		elif self.moving_left:
+			self.rect.x -= 1
 
 	def blitme(self):
 		""" drawing ship in current position """
